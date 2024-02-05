72bf5386a8ce8f80bf0e98e7983eff577ce1fbd4
finscrape <finscrape@gmail.com>
2024-02-05 01:56:12 +0000

    Forkedd and commit

---
+++ planners/planners/spiders/ambervalley.py
@@ -0,0 +1,207 @@
+from typing import Iterable
+import scrapy
+import requests
+from scrapy.http import Request
+from planners.util import dates_stry
+from time import sleep
+from scrapy import Selector
+import json
+class AmbervalleySpider(scrapy.Spider):
+    name = "ambervalley"
+    # allowed_domains = ["a.com"]
+    # start_urls = ["https://a.com"]
+
+    def start_requests(self):
+        yield scrapy.Request(url="https://www.ebay.com",method="GET")
+
+    def parse(self,response):
+
+        import requests
+
+        url = "https://info.ambervalley.gov.uk/WebServices/AVBCFeeds/DevConJSON.asmx/PlanAppsAllValidNonDetermined"
+
+        payload = "wardCode=&parishCode="
+        headers = {
+        'Accept': ' application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding': ' gzip, deflate, br',
+        'Accept-Language': ' en-US,en;q=0.9',
+        'Connection': ' keep-alive',
+        'Content-Length': ' 21',
+        'Content-Type': ' application/x-www-form-urlencoded; charset=UTF-8'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        page = response.text
+        try:
+            html = json.loads(page)
+        except:
+            pass
+
+
+        if html:
+            for i in html:
+                ref = i.get("refVal")
+                ref = ref.replace("/",r"%2F")
+                abs_i = f'https://info.ambervalley.gov.uk/WebServices/AVBCFeeds/DevConJSON.asmx/GetPlanAppDetails?refVal={ref}'
+                yield scrapy.Request(url=abs_i,callback=self.details,meta={'ref':ref})
+                sleep(1)
+        
+
+
+       
+        start = 0
+        
+        for d in dates_stry[0:-1]:
+
+            start += 1
+            fd = d
+            nd = dates_stry[start]
+            url = "https://info.ambervalley.gov.uk/WebServices/AVBCFeeds/DevConJSON.asmx/PlanAppsDetermined"
+
+            payload = f"wardCode=&parishCode=&fromDate={fd}&toDate={nd}"
+            headers = {
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Connection': 'keep-alive',
+            'Content-Length': '59',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-site'
+            }
+
+            response = requests.request("POST", url, headers=headers, data=payload)
+            page = response.text
+            try:
+                html = json.loads(page)
+            except:
+                continue
+
+
+            if html:
+                for i in html:
+                    ref = i.get("refVal")
+                    ref = ref.replace("/",r"%2F")
+                    abs_i = f'https://info.ambervalley.gov.uk/WebServices/AVBCFeeds/DevConJSON.asmx/GetPlanAppDetails?refVal={ref}'
+                    yield scrapy.Request(url=abs_i,callback=self.details,meta={'ref':ref})
+                    sleep(1)
+            
+
+    def details(self,response):
+        ref = response.meta['ref']
+        page = response.body
+        try:
+            html = json.loads(page)
+        except:
+            pass
+
+        if html:
+            each = {}
+            for key, value in html.items():
+
+                each[key] = value
+            yield scrapy.Request(url=f'https://info.ambervalley.gov.uk/WebServices/AVBCFeeds/DemocracyJSON.asmx/GetCommitteeDocsByPlanAppRef?refVal={ref}&publicOnly=true',callback=self.com,meta={'each':each,'ref':ref})
+
+    def com(self,response):
+        ref = response.meta['ref']
+        each = response.meta['each']
+        page = response.body
+        try:
+            html = json.loads(page)
+        except:
+            pass
+
+        if html:
+            
+            each['committeeDoc'] = html
+        yield scrapy.Request(url=f'https://info.ambervalley.gov.uk/WebServices/AVBCFeeds/IdoxEDMJSON.asmx/GetIdoxEDMDocListForCase?refVal={ref}&docApplication=planning',callback=self.idox,meta={'each':each,'ref':ref})
+
+    def idox(self,response):
+        ref = response.meta['ref']
+        each = response.meta['each']
+        page = response.body
+        try:
+            html = json.loads(page)
+        except:
+            pass
+
+        if html:
+            
+            each['idoxEdmDocListForCase'] = html
+        yield scrapy.Request(url=f'https://info.ambervalley.gov.uk/WebServices/AVBCFeeds/DevConJSON.asmx/GetApplicableConstraintsForPlanApp?refVal={ref}',callback=self.con,meta={'each':each,'ref':ref})
+
+    def con(self,response):
+        ref = response.meta['ref']
+        each = response.meta['each']
+        page = response.body
+        try:
+            html = json.loads(page)
+        except:
+            pass
+
+        if html:
+            
+            each['applicableConstraints'] = html
+        yield scrapy.Request(url=f'https://info.ambervalley.gov.uk/WebServices/AVBCFeeds/DevConJSON.asmx/GetConsultees?refVal={ref}&consulteeType=consultees',callback=self.consa,meta={'each':each,'ref':ref})
+
+    def consa(self,response):
+        ref = response.meta['ref']
+        each = response.meta['each']
+        page = response.body
+        try:
+            html = json.loads(page)
+        except:
+            pass
+
+        if html:
+            
+            each['consultees'] = html
+        yield scrapy.Request(url=f'https://info.ambervalley.gov.uk/WebServices/AVBCFeeds/DevConJSON.asmx/GetConsultees?refVal={ref}&consulteeType=pressList',callback=self.const,meta={'each':each,'ref':ref})
+
+    def const(self,response):
+        ref = response.meta['ref']
+        each = response.meta['each']
+        page = response.body
+        try:
+            html = json.loads(page)
+        except:
+            pass
+
+        if html:
+            
+            each['consulteesTypePressList'] = html
+        yield scrapy.Request(url=f'https://info.ambervalley.gov.uk/WebServices/AVBCFeeds/DevConJSON.asmx/GetConsultees?refVal={ref}&consulteeType=siteNotice',callback=self.consty,meta={'each':each,'ref':ref})
+
+    def consty(self,response):
+        ref = response.meta['ref']
+        each = response.meta['each']
+        page = response.body
+        try:
+            html = json.loads(page)
+        except:
+            pass
+
+        if html:
+            
+            each['consulteesTypeSiteNotice'] = html
+        yield scrapy.Request(url=f'https://info.ambervalley.gov.uk/WebServices/AVBCFeeds/DevConJSON.asmx/GetConsultees?refVal={ref}&consulteeType=neighbour',callback=self.conne,meta={'each':each,'ref':ref})
+
+    def conne(self,response):
+        ref = response.meta['ref']
+        each = response.meta['each']
+        page = response.body
+        try:
+            html = json.loads(page)
+        except:
+            pass
+
+        if html:
+            
+            each['consulteesTypeNeighbours'] = html
+        yield each
+        
+
+    
+    
---
+++ planners/planners/spiders/nuneatonandbedworth.py
@@ -0,0 +1,112 @@
+import scrapy
+from planners.util import dates_strx
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# import ddddocr
+
+
+# options = FirefoxOptions()
+# options.add_argument("--headless")
+# from time import sleep
+# from scrapy import Selector
+# from scrapy import signals
+
+
+# from re import sub
+# global driver
+
+# from pydispatch import dispatcher
+
+# def stripper(second):
+#     if second:
+#         second = second.replace('\r','').replace('\n','').replace('\t','').replace(":","").strip()
+#     return second
+                
+    
+
+
+# # Define a function to convert a string to camel case
+# def camel_case(s):
+#     # Use regular expression substitution to replace underscores and hyphens with spaces,
+#     # then title case the string (capitalize the first letter of each word), and remove spaces
+#     s = sub(r"(_|-)+", " ", s).title().replace(" ", "")
+    
+#     # Join the string, ensuring the first letter is lowercase
+#     return ''.join([s[0].lower(), s[1:]])
+
+
+# class NuneatonandbedworthSpider(scrapy.Spider):
+#     name = "nuneatonandbedworth"
+#     # allowed_domains = ["a.com"]
+#     # start_urls = ["https://a.com"]
+
+#     def __init__(self, name=None, **kwargs):
+#         self.listhref  = []
+#         self.plan = 0
+#         self.total = []
+#         self.driver = webdriver.Firefox(options=options,service=Service(GeckoDriverManager().install()))
+#         self.driver.maximize_window()
+
+#         self.drivera = webdriver.Firefox(options=options,service=Service(GeckoDriverManager().install()))
+#         self.drivera.maximize_window()
+#         dispatcher.connect(self.spider_closed,signals.spider_closed)
+
+#     def start_requests(self):
+#         yield scrapy.Request(url="https://www.ebay.com",method="GET")
+
+#     def parse(self,response):
+#             self.driver.get("https://customer.nuneatonandbedworth.gov.uk/en/AchieveForms/?form_uri=sandbox-publish://AF-Process-bb4f1551-25f7-4c9d-9504-359555b4764c/AF-Stage-52bb890a-7580-4889-8b19-31d27fd8a3bd/definition.json&redirectlink=/en&cancelRedirectLink=/en&consentMessage=yes&noLoginPrompt=1")
+#             sleep(10)
+#             try:
+#                 self.driver.find_element('xpath',"//label[contains(text(),'Search by application number')]").click()
+#             except:
+#                 pass
+            
+#             sleep(3)
+            
+#             for i in list(range(0,1)):
+#                 i = str(i)
+#                 if len(i) == 1:
+#                     i = f'00000{i}'
+#                 elif len(i) == 2:
+#                     i = f'0000{i}'
+#                 elif len(i) == 3:
+#                     i = f'000{i}'
+#                 elif len(i) == 4:
+#                     i = f'00{i}'
+#                 elif len(i) == 5:
+#                     i = f'0{i}'
+#                 try:
+#                     #date picker
+#                     fromDate = self.driver.find_element("xpath","//input[@id='applicationReferenc']")
+
+#                     fromDate.clear()
+
+#                     fromDate.send_keys("040062")
+#                     self.driver.find_element("xpath","//button[@id='PLsearch']").click()
+#                     sleep(2)
+
+                    
+                    
+                            
+#                 except:
+#                     continue
+
+            
+                
+                
+#                 captcha = self.driver.find_element("xpath", "//textarea[@id='concatDetails']")
+#                 captcha.screenshot(f'captcha.png')
+
+#                 ocr = ddddocr.DdddOcr()
+#                 # open and read the image
+#                 with open(f'captcha.png', 'rb') as f:
+#                     img_bytes = f.read()
+
+#                 res = ocr.classification(img_bytes)
+#                 print(res.upper())
